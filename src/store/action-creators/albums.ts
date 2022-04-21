a16c60a6fc15283d87de2b35f9db7c608ff04d83
Vlad Platov <vladplatov3@yandex.ru>
2022-04-21 22:16:42 +0500

    deleted useless try catch

--- src/store/action-creators/albums.ts
+++ src/store/action-creators/albums.ts
@@ -4,18 +4,17 @@ import { AlbumAction, AlbumsActionTypes } from "../../types/albums";
 
 export const fetchAlbums = () => {
     return async (dispatch: Dispatch<AlbumAction>) => {
-        try {
-            dispatch({ type: AlbumsActionTypes.FETCH_ALBUMS })
-            await axios
-                .get('https://jsonplaceholder.typicode.com/albums')
-                .then(response => {
-                    dispatch({ type: AlbumsActionTypes.FETCH_ALBUMS_SUCCESS, albums: response.data })
-                })
-        } catch (e) {
-            dispatch({
-                type: AlbumsActionTypes.FETCH_ALBUMS_ERROR,
-                error: 'Ошибка при загрузке данных с сервера'
+        dispatch({ type: AlbumsActionTypes.FETCH_ALBUMS })
+        await axios
+            .get('https://jsonplaceholder.typicode.com/albums')
+            .then(response => {
+                dispatch({ type: AlbumsActionTypes.FETCH_ALBUMS_SUCCESS, albums: response.data })
             })
-        }
+            .catch(e => {
+                dispatch({
+                    type: AlbumsActionTypes.FETCH_ALBUMS_ERROR,
+                    error: 'Ошибка при загрузке данных с сервера'
+                })
+            })
     }
 }
